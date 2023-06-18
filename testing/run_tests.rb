165528d739729c864f2563a6ea31250b04768b29
pes18fan <pes18fan@disr.it>
2023-06-18 19:58:39 +0545

    [test] added ability to test error messages in tester

--- testing/run_tests.rb
+++ testing/run_tests.rb
@@ -1,3 +1,5 @@
+require "open3"
+
 test_folder = "__tests__"
 $compiler = "../bin/rel/zen"
 
@@ -20,11 +22,11 @@ def test(folder)
       test(file_path)
     elsif file_path.end_with?(".zn")
       print "Testing #{TEXT_BOLD}#{file_path}#{RESET}: "
-      output = `#{$compiler} #{file_path}`
+      output, error, status = capture_output("#{$compiler} #{file_path}")
 
-      expect = read_expected_output(file_path)
+      expect, expected_err, wants_err = read_expected_output(file_path)
 
-      if $?.success?
+      if status == 0
         if multiline_output_match?(output, expect)
           puts "#{COL_GREEN}PASSED#{RESET} with expected output"
         else
@@ -33,8 +35,16 @@ def test(folder)
           puts "Actual:\n#{output}"
         end
       else
-        puts "#{COL_RED}FAILED#{RESET} with error"
-        exit 1
+        if wants_err
+          if error.strip.include?(expected_err)
+            puts "#{COL_GREEN}PASSED#{RESET} with expected error"
+          else
+            puts "#{COL_RED}FAILED#{RESET} with unexpected error"
+            puts "Expected:\n#{expected_err}"
+            puts "Got:\n#{error}"
+            exit 1
+          end
+        end
       end
     end
   end
@@ -42,21 +52,35 @@ end
 
 def read_expected_output(path)
   expected_output = ""
+  expected_error = ""
   read_output = false
+  read_error = false
+  wants_error = false
+
   File.open(path, "r") do |file|
     file.each_line do |line|
-      if line.start_with?("// expect:")
+      if line.strip == "// expect:"
         read_output = true
         next
-      elsif read_output && line.start_with?("// end expect")
+      elsif read_output && line.strip == "// end expect"
         read_output = false
         break
+      elsif line.strip == "// expect error:"
+        read_error = true
+        wants_error = true
+        next
+      elsif read_error && line.strip == "// end expect error"
+        read_error = false
+        next
+      elsif read_error && line.start_with?("// ")
+        expected_error += line.sub("// ", "")
       elsif read_output && line.start_with?("// ")
-        expected_output += line.sub("// ", "").strip + "\n"
+        expected_output += line.sub("// ", "")
       end
     end
   end
-  expected_output
+
+  [expected_output.strip, expected_error.strip, wants_error]
 end
 
 def multiline_output_match?(actual_output, expected_output)
@@ -64,6 +88,21 @@ def multiline_output_match?(actual_output, expected_output)
   expected_lines = expected_output.strip.split("\n")
 
   actual_lines.size == expected_lines.size && actual_lines.zip(expected_lines).all? { |a, e| a.strip == e.strip }
+end
+
+# Capture stderr and stdout seperately
+def capture_output(command)
+  output = ""
+  error = ""
+  exit_status = 0
+
+  Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
+    output = stdout.read
+    error = stderr.read
+    exit_status = wait_thr.value
+  end
+
+  [output, error, exit_status]
 end
 
 test(test_folder)
