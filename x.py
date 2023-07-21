7f6ae1a57bbc7c62fe1d0adf9744427f35f895ea
pes18fan <pes18fan@disr.it>
2023-07-21 21:13:17 +0545

    [refactor] replace just with python
    This commit replaces the current justfile-based build system with a Python script.
    Just was far less verbose and very easy to work with, but Python has much better cross
    platform compatibility and it just works anywhere; additionally it has a powerful
    argument parser that helps a lot. I might change this decision later on, but for now,
    Python script it is.

---
+++ x.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+import argparse
+import subprocess
+import sys
+
+OC = "odin"
+ProcError = subprocess.CalledProcessError
+
+def create_debug_build():
+    try:
+        print("Compiling the debug build..")
+        subprocess.run(f"{OC} build src/ -out:bin/dbg/dzen -debug -o:none".split(),
+                       check=True)
+    except ProcError as e:
+        print(f"Error while creating debug build: {e}", file=sys.stderr)
+        exit(1)
+
+def create_release_build():
+    try:
+        print("Compiling in release mode..")
+        subprocess.run(f"{OC} build src/ -out:bin/rel/zen -o:speed".split(),
+                       check=True)
+    except ProcError as e:
+        print(f"Error while creating release build: {e}", file=sys.stderr)
+        exit(1)
+
+def benchmark(command, against):
+    try:
+        subprocess.run(f"""hyperfine -N --warmup 3 --min-runs 5\
+                           bin/rel/zen {command} {against}""".split(),
+                       check=True)
+    except FileNotFoundError:
+        print("hyperfine needed to benchmark", file=sys.stderr)
+        exit(1)
+
+def clean():
+    subprocess.run("rm -rf bin/**".split(), check=True)
+    print("cleaned build artifacts.")
+
+def generate_docs():
+    with open("doc/docs.txt", "w+") as doc_file:
+        try:
+            subprocess.run(f"{OC} doc src/".split(), stdout=doc_file, 
+                           check=True)
+        except ProcError as e:
+            print(f"Error when generating docs: {e}", file=system)
+            exit(1)
+
+    print("docs generated at doc/docs.txt")
+
+def test():
+    print("Running unit tests:")
+    try:
+        subprocess.run(f"{OC} test ../../src/".split(), cwd="bin/test/", 
+                       check=True)
+    except ProcError as e:
+        print(f"Error when running unit tests: {e}", file=sys.stderr)
+        exit(1)
+
+    print("")
+
+    print("Running end-to-end tests:")
+    try:
+        subprocess.run(f"ruby ./run_tests.rb".split(), cwd="test/", check=True)
+    except FileNotFoundError:
+        print("Ruby needed to run the tests!", file=sys.stderr)
+        exit(1)
+    except ProcError as e:
+        print(f"Error during e2e tests: {e}", file=sys.stderr)
+        exit(1)
+
+def run_build(args):
+    try:
+        subprocess.run([f"./bin/dbg/dzen"] + args.split(), check=True)
+    except ProcError as e:
+        print(f"Error running the build: {e}", file=sys.stderr)
+        exit(1)
+
+def main():
+    try:
+        subprocess.run(f"{OC} version".split(),
+                       stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL,
+                       check=True)
+    except ProcError:
+        print(f"You need Odin to build!")
+        exit(1)
+    
+    parser = argparse.ArgumentParser(description="zen build system")
+    subparsers = parser.add_subparsers(dest="command")
+
+    # debug build
+    dbg_parser = subparsers.add_parser("dbg", help="create a debug build")
+    dbg_parser.set_defaults(func=create_debug_build)
+
+    # release build
+    rel_parser = subparsers.add_parser("rel", help="create a release build")
+    rel_parser.set_defaults(func=create_release_build)
+
+    # benchmark
+    bench_parser = subparsers.add_parser("bench", 
+                                         help="benchmark the release against another command")
+    bench_parser.add_argument("--args", 
+                              required=True,
+                              help="args to pass to the release build and the other command")
+    bench_parser.add_argument("--against", 
+                              required=True,
+                              help="command to benchmark against")
+    bench_parser.set_defaults(func=lambda args: benchmark(args.command, args.against))
+
+    # clean
+    clean_parser = subparsers.add_parser("clean", help="clean build artifacts")
+    clean_parser.set_defaults(func=clean)
+
+    # doc generator
+    doc_parser = subparsers.add_parser("doc", 
+                                       help="generate documentation in the doc/ folder")
+    doc_parser.set_defaults(func=generate_docs)
+
+    # Test all command
+    test_parser = subparsers.add_parser("test", help="run all tests")
+    test_parser.set_defaults(func=test)
+
+    # Run program command
+    run_parser = subparsers.add_parser("run", help="run the debug build")
+    run_parser.add_argument("--args", default="", 
+                            help="optional args to pass to the program")
+    run_parser.set_defaults(func=lambda args: run_build(args.args))
+
+    args = parser.parse_args()
+    if not hasattr(args, "func"):
+        parser.print_help()
+        exit(1)
+
+    if args.func.__code__.co_argcount > 0:
+        args.func(args)
+    else:
+        args.func()
+
+if __name__ == "__main__":
+    main()
